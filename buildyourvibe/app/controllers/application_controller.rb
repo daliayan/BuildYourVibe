192d248caf2c4a33e33e512c21692fa09ba318fd
daliayan <daliayan5@gmail.com>
2021-06-15 17:55:32 -0400

    Trouble with Puma

--- buildyourvibe/app/controllers/application_controller.rb
+++ buildyourvibe/app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
-require './config/environment'
-
-class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get "/" do
-    erb :welcome
-  end
+class ApplicationController < ActionController::Base
 
 end
