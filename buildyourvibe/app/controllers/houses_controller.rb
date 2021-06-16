df20eb95c1336ff9e892502febd9a5794ca68be9
daliayan <daliayan5@gmail.com>
2021-06-16 17:06:02 -0400

    Skeleton for houses/rooms/comments controller

--- buildyourvibe/app/controllers/houses_controller.rb
+++ buildyourvibe/app/controllers/houses_controller.rb
@@ -1,3 +1,24 @@
 class HousesController < ApplicationController
 
+    def index
+        @houses = House.all
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+    
+    def destroy
+    end
+
+    private
+
+    def house_params
+    end
+
 end
--- buildyourvibe/app/controllers/rooms_controller.rb
+++ buildyourvibe/app/controllers/rooms_controller.rb
@@ -1,3 +1,30 @@
 class RoomsController < ApplicationController
 
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    
+    def room_params
+    end
+
+
 end
