496efdf7e8e1df0e4bb74e8b9a41bcafb421980b
daliayan <daliayan5@gmail.com>
2021-06-16 16:22:24 -0400

    Users view files and Sessions view files - needs forms >> built out their controllers >> played with app layout >> routes are working so far

--- buildyourvibe/app/controllers/sessions_controller.rb
+++ buildyourvibe/app/controllers/sessions_controller.rb
@@ -1,2 +1,27 @@
 class SessionsController < ApplicationController
+
+    def welcome
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        user = User.find_by_username(params[:username])
+
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Wrong login information. Please try again!"
+            redirect_to "/login"
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to root_path
+    end
+    
 end
--- buildyourvibe/config/routes.rb
+++ buildyourvibe/config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
     root to: 'sessions#welcome'
 
+    
+    get '/signup' => 'users#new'
+    post '/signup' => 'users#create'
+
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
+    
     delete '/logout', to: 'sessions#destroy'
-
-    get '/signup', to: 'users#new'
-    post '/signup', to: 'users#create'
-    resources :users
-
-    resources :houses
-    resources :rooms 
 
     resources :houses do
         resources :comments
