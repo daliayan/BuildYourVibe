061e07607641015ce028f27f5bb9e5799d9e214e
daliayan <daliayan5@gmail.com>
2021-06-19 11:59:03 -0400

    Build out some house views / working on rooms controller / havent tried runnign puma+shotgun

--- buildyourvibe/app/controllers/rooms_controller.rb
+++ buildyourvibe/app/controllers/rooms_controller.rb
@@ -1,15 +1,29 @@
 class RoomsController < ApplicationController
 
     def index
+        @rooms = Room.all
     end
 
     def show
+        @room = Room.find(params[:id])
     end
 
     def new
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @room = @user.rooms.build
+        else
+            @room = Room.new
+            @room.build_house
+        end
     end
 
     def create
+        @room = current_user.rooms.build(room_params)
+        if @room.save
+            redirect_to rooms_path
+        else
+            render :new
+        end
     end
 
     def edit
@@ -24,6 +38,7 @@ class RoomsController < ApplicationController
     private
     
     def room_params
+        params.require(:room).permit(:name, :room_description, :windows, :hex, :user_id, :house_id)
     end
 
 
