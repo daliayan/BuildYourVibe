0055a83f89816ca223f4ac9038c47ad7456a9a22
daliayan <daliayan5@gmail.com>
2021-06-22 10:23:03 -0400

    Puma working / working on session/user controller to save users

--- buildyourvibe/config/routes.rb
+++ buildyourvibe/config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
     root to: 'sessions#welcome'
 
-    
-    get '/signup' => 'users#new'
-    post '/signup' => 'users#create'
-
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
-    
     delete '/logout', to: 'sessions#destroy'
+
+    get '/signup' => 'users#new'
+    post '/signup' => 'users#create'
+    resources :users
 
     resources :houses do
         resources :comments
