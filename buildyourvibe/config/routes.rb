a083839b7f233a9d4f8232be466f19ecff9905d5
daliayan <daliayan5@gmail.com>
2021-06-22 15:56:26 -0400

    Bin/rails not working

--- buildyourvibe/config/routes.rb
+++ buildyourvibe/config/routes.rb
@@ -1,22 +1,17 @@
 Rails.application.routes.draw do
     root 'sessions#welcome'
 
-    resources :users do
-        resources :houses #shallow: true routing to avoid deep nesting
-    end
-
     get '/signup' => 'users#new'
     post '/signup' => 'users#create'
 
-    # get '/login' => 'sessions#new'
-    # post '/login' => 'sessions#create'
-    resources :sessions, only: [:create]
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
 
     get '/logout' => 'sessions#destroy'
 
-    # resources :users do
-    #     resources :houses #shallow: true routing to avoid deep nesting
-    # end
+    resources :users do
+        resources :houses #shallow: true routing to avoid deep nesting
+    end
 
     resources :rooms do
         resources :comments
